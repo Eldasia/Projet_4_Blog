a956de7e3bdd35366f848f142e5c49991bbd8e41
Maureen <maureen.bruihier@yahoo.fr>
2020-05-19 10:23:08 +0200

    Ajout de commentaires sur les classes Validation et View

--- lib/Validation.php
+++ lib/Validation.php
@@ -4,6 +4,12 @@ namespace MaureenBruihier\Projet4\Lib;
 
 use \MaureenBruihier\Projet4\model\Manager;
 
+/**
+ * La classe Validation permet de gérer la vérification d'un formulaire
+ * 
+ * @author Maureen <maureen.bruihier@yahoo.fr>
+ */
+
 class Validation 
 {
     public static $instance;
@@ -21,7 +27,17 @@ class Validation
             unset($_SESSION['validation_errors']);
         }
     }
-
+        
+    /**
+     * make
+     *
+     *  Permet de travailler tout le temps sur la même instance de Validation
+     * 
+     * @param  array $data Tableau de données à vérifier
+     * @param  array $rules Tableau des règles
+     * 
+     * @return void
+     */
     public static function make(array $data, array $rules) 
     {
         if (self::$instance == null)
@@ -31,19 +47,49 @@ class Validation
 
         return self::$instance;
     }
-
+        
+    /**
+     * redirectWithErrors
+     *
+     * Permet une redirection en gardant le message d'erreur pour pouvoir l'afficher
+     * 
+     * @param  string $location Lien vers la page de redirection
+     * 
+     * @return void
+     */
     public function redirectWithErrors(string $location) : void
     {
         $_SESSION['validation_errors'] = $this->errors;
         header('Location: ' . $location);
         exit;
     }
-
+    
+    /**
+     * hasError
+     *
+     * Renvoie un booléen pour savoir si une erreur pour la clé a été enregistrée
+     * 
+     * @param  string $key Clé permettant de vérifier si il existe une erreur 
+     * 
+     * @return bool
+     */
     public function hasError(string $key) : bool
     {
         return array_key_exists($key, $this->errors);
     }
-
+    
+    /**
+     * getError
+     * 
+     * Vérifie si il y a une erreur existante pour la clé passée en paramètre 
+     * et envoie un message d'erreur en fonction de la règle sur laquelle il y a une erreur
+     *
+     * @param  string $key Clé permettant de vérifier si il existe une erreur
+     * 
+     * @return string si il y a une erreur
+     * sinon
+     * @return null
+     */
     public function getError(string $key) : ?string
     {
         if ($this->hasError($key))
@@ -77,7 +123,19 @@ class Validation
 
         return null;
     }
-
+    
+    /**
+     * getOldValue
+     * 
+     * Permet de récupérer la valeur de la clé enregistrée pour pouvoir la restituer
+     * et faire gagner du temps à l'utilisateur sur la correction de son erreur
+     *
+     * @param  string $key Clé permettant de vérifier si il existe une erreur
+     * 
+     * @return string Retourne l'ancienne valeur du champs vérifié si il y a une erreur
+     * sinon
+     * @return null
+     */
     public function getOldValue(string $key) : ?string
     {
         if ($this->hasError($key))
@@ -87,7 +145,14 @@ class Validation
 
         return null;
     }
-
+    
+    /**
+     * isValid
+     *
+     * Vérifie que les données envoyées sont valides si elles ont une entrée dans le tableau des règles
+     * 
+     * @return bool Renvoie true si il n'y a pas d'erreurs, false si il y en a
+     */
     public function isValid() : bool
     {
         foreach ($this->data as $key=>$value)
@@ -100,7 +165,18 @@ class Validation
 
         return count($this->errors) == 0;
     }
-
+    
+    /**
+     * validateAttr
+     * 
+     * Valide chaque clé et sa value en fonction des règles données
+     * Si la clé et sa value ne sont pas valides, crée une erreur dans le tableau d'erreurs
+     *
+     * @param  mixed $attr Nom du champ à vérifier
+     * @param  mixed $value Valeur du champ à vérifier
+     * 
+     * @return void
+     */
     protected function validateAttr($attr, $value) : void
     {
         foreach ($this->rules[$attr] as $rule)
@@ -114,7 +190,17 @@ class Validation
 
         return;
     }
-
+    
+    /**
+     * rule
+     * 
+     * Appelle les méthodes assignées pour chaque règle
+     *
+     * @param  mixed $rule La règle à vérifier
+     * @param  mixed $value Valeur à valider
+     * 
+     * @return bool Hérité de la méthode qui a été return
+     */
     protected function rule($rule, $value) : bool
     {
         if (stripos($rule, ':') === false) 
@@ -128,22 +214,62 @@ class Validation
 
         return $this->{$method}($value, $parts[1]);
     }
-
+    
+    /**
+     * ruleRequired
+     *
+     * Vérifie et retourne true si le paramètre entré n'est pas vide
+     * 
+     * @param  mixed $value Valeur du champ à valider
+     * 
+     * @return bool
+     */
     protected function ruleRequired($value) : bool
     {
         return !empty($value);
     }
-
+    
+    /**
+     * ruleMin
+     * 
+     * Retourne true si la chaine de caractère donnée est plus grande ou égale au paramètre de la règle
+     *
+     * @param  mixed $value Valeur du champ à valider
+     * @param  mixed $param Paramètre de la règle
+     * 
+     * @return bool
+     */
     protected function ruleMin($value, $param) : bool
     {
         return strlen($value) >= $param;
     }
-
+    
+    /**
+     * ruleMax
+     *
+     * Retourne true si la chaine de caractère donnée est plus petite ou égale au paramètre de la règle
+     * 
+     * @param  mixed $value Valeur du champ à valider
+     * @param  mixed $param Paramètre de la règle
+     * 
+     * @return bool
+     */
     protected function ruleMax($value, $param) : bool
     {
         return strlen($value) <= $param;
     }
-
+    
+    /**
+     * ruleExist
+     * 
+     * Vérifie grâce aux paramètres de la règle que le contenu du champ est existant dans la base de données
+     * Renvoie true si il est dans la BDD
+     *
+     * @param  mixed $value Valeur du champ à valider
+     * @param  mixed $param Paramètre de la règle
+     * 
+     * @return bool
+     */
     protected function ruleExist($value, $param) : bool
     {
         $params = explode(',', $param);
--- lib/View.php
+++ lib/View.php
@@ -4,6 +4,13 @@ namespace MaureenBruihier\Projet4\Lib;
 
 use MaureenBruihier\Projet4\lib\Validation;
 
+/**
+ * La classe Views permet de gérer les vues sur un projet et de les appeler si besoin
+ * Elle permet également de gérer le contenu de la page pour le générer en même temps que la page
+ * 
+ * @author Maureen <maureen.bruihier@yahoo.fr>
+ */
+
 class View 
 {
     public $view;
@@ -16,12 +23,30 @@ class View
         $this->params = $params;
         $this->params['validation'] = Validation::$instance ?? new Validation;
     }
-
-    public static function make($view, array $params = [])
+    
+    /**
+     * make
+     * 
+     * Génère une vue avec les paramètres donnés
+     *
+     * @param  string $view Lien de la vue à retourner
+     * @param  array $params Les paramètres à afficher, comme le titre ou le contenu
+     * 
+     * @return void
+     */
+    public static function make(string $view, array $params = [])
     {
         return (new static($view, $params))->send();
     }
-
+    
+    /**
+     * send
+     * 
+     * Extrait le titre du tableau de paramètres
+     * Insère le titre, le contenu et la vue(tous les deux stocké dans la variable $content) souhaitée dans le template principal du site
+     *
+     * @return void
+     */
     public function send ()
     {
         ob_start();
@@ -30,7 +55,14 @@ class View
         require 'views/template.php';
         return ob_get_clean();
     }
-
+    
+    /**
+     * compile
+     * 
+     * Extrait le contenu du tableau de paramètre et l'insère dans la vue que l'on veut
+     *
+     * @return void
+     */
     protected function compile () 
     {
         ob_start();
